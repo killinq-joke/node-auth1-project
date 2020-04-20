0ce6191e1f4afc3054617fd126f284bb51649b50
killinq-joke <zakadmaster@gmail.com>
2020-04-20 21:41:32 +0200

    Addition of hashing

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,20 @@
 const router = require("express").Router();
+const md = require("../users/user-model");
 
-router.post("/register", (req, res) => {});
+const bc = require("bcryptjs");
+
+router.post("/api/register", (req, res) => {
+  const { username, password } = req.body;
+  const salt = Number(process.env.SALT)
+  const hash = bc.hashSync(password, salt)
+   md.add({ username, password: hash })
+    .then((response) => {
+      res.status(201).json(response);
+    })
+    .catch((err) => {
+      res.status(500).json({ errorMessage: err.message });
+    });
+});
 
 router.post("/login", (req, res) => {});
 
