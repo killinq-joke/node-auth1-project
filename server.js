1236311849d3bc80aa30cdfdb8efb0d049216bc5
killinq-joke <zakadmaster@gmail.com>
2020-04-20 20:56:25 +0200

    Addition of routers

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require("express")
 const cors = require("cors")
 const helmet = require("helmet")
 
+const userRouter = require("./users/user-router");
+const authRouter = require("./auth/auth-router")
+
 const server = require(express())
 
 server.use(express.json())
 server.use(cors())
 server.use(helmet())
+server.use(userRouter)
+server.use(authRouter);
 
 module.exports = server
