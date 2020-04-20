359e92849335eec9be514710f7f484ef248dd55d
killinq-joke <zakadmaster@gmail.com>
2020-04-20 21:59:21 +0200

    Addition of session

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const session = require("express-session");
 
 const userRouter = require("./users/user-router");
 const authRouter = require("./auth/auth-router");
 
 const server = express();
 
+const sessionConfig = {
+  name: "bruh",
+  secret: process.env.SECRET || "secret",
+  resave: false,
+  saveUninitialized: process.env.SEND_COOKIES || true,
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: process.env.SECURE || false,
+    httpOnly: true,
+  },
+};
+
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+server.use(session(sessionConfig))
 server.use(userRouter);
 server.use(authRouter);
 
